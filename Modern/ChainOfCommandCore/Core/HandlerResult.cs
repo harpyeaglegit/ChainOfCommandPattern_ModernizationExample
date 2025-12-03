2753faaa61b7406ee02dcb9fecaeebbb08c7b0d2
winuser <winuser@local>
2025-12-03 14:18:43 -0600

    Update ChainOfCommandCore project - Add Core/HandlerResult.cs to contain result definition.

---
+++ Modern/ChainOfCommandCore/Core/HandlerResult.cs
@@ -0,0 +1,20 @@
+﻿namespace ChainOfCommandCore.Core
+{
+    /// <summary>
+    /// HandlerResult enumeration is a list a possible status values to be 
+    /// returned by the IChainHandler.ProcessRequest() method call.
+    /// </summary>
+    public enum HandlerResult
+    {
+        /// <summary>
+        /// Indicates the handler has completely handled the request
+        /// and no further processing is needed (processing should stop).
+        /// </summary>
+        CHAIN_DATA_HANDLED,
+        /// <summary>
+        /// Indicates that the handler is not completely handled,
+        /// and the next handler in the chain should be called.
+        /// </summary>
+        CHAIN_DATA_NOT_HANDLED
+    };
+}
